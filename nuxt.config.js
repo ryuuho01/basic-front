bd7e7ae79bc67766c40999b4bc22044d7fe5aff3
ryuuho <krjima@yahoo.co.jp>
2021-12-11 18:06:39 +0900

    npm run generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,15 +49,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    done(generator){
-      // Copy dist files to public/_nuxt
-      if (generator.nuxt.options.dev === false && generator.nuxt.options.mode === 'spa') {
-        const publicDir = join(generator.nuxt.options.rootDir, 'public', '_nuxt')
-          removeSync(publicDir)
-          copySync(join(generator.nuxt.options.generate.dir, '_nuxt'), publicDir)
-          copySync(join(generator.nuxt.options.generate.dir, '200.html'), join(publicDir, 'index.html'))
-      }
-    }
-    
   },
 }
